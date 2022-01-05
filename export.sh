b55d3a2f38d375511b14122f04b78494cb1bb094
fimtow <62258310+fimtow@users.noreply.github.com>
2022-01-06 00:13:21 +0100

    added cassandra deployement

--- export.sh
+++ export.sh
@@ -2,3 +2,4 @@ export BUCKET="stdt-kops-storage-13205"
 export CLUSTER_NAME="psdtd.k8s.local"
 export KOPS_FEATURE_FLAGS=AlphaAllowGCE
 export KOPS_STATE_STORE=gs://${BUCKET}/
+export PROJECT=$(gcloud config get-value project)
