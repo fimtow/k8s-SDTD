9f2aa306ba9fe7ee391dd7a9fa4ec7da4603722d
fimtow <62258310+fimtow@users.noreply.github.com>
2022-01-18 11:20:44 +0100

    temporary fix to deployment delete issue

--- delete.sh
+++ delete.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-kubectl delete -f deployement
+#kubectl delete -f deployement
 kops delete cluster ${CLUSTER_NAME} --yes
 sed -i 's/'"${BUCKET}"'/xxxxxxxxxxx/' ${CLUSTER_NAME}.yaml
 sed -i 's/'"${PROJECT}"'/yyyyyyyyyyy/' ${CLUSTER_NAME}.yaml
