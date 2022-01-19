00726d2a92803e5295392a5458cec3386f0b156c
fimtow <62258310+fimtow@users.noreply.github.com>
2022-01-19 19:08:24 +0100

    minor fixes

--- delete.sh
+++ delete.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 #kubectl delete -f deployement
+kubectl delete -f deployement/kafka.yaml 
+kubectl delete -f deployement/spark.yaml
+kubectl delete -f deployement/spark2.yaml 
+#kubectl delete service -n spark driverui 
+#kubectl delete service -n spark2 driverui
+kubectl delete -f deployement/visualisation.yaml
 kops delete cluster ${CLUSTER_NAME} --yes
 sed -i 's/'"${BUCKET}"'/xxxxxxxxxxx/' ${CLUSTER_NAME}.yaml
 sed -i 's/'"${PROJECT}"'/yyyyyyyyyyy/' ${CLUSTER_NAME}.yaml
