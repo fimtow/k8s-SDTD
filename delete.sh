59b4bed428a072f5bea644baa1e336b10607e4dc
fimtow <62258310+fimtow@users.noreply.github.com>
2022-01-20 17:11:02 +0100

    spark delete fix

--- delete.sh
+++ delete.sh
@@ -2,10 +2,10 @@
 set -e
 #kubectl delete -f deployement
 kubectl delete -f deployement/kafka.yaml 
-kubectl delete -f deployement/spark.yaml
-kubectl delete -f deployement/spark2.yaml 
-#kubectl delete service -n spark driverui 
-#kubectl delete service -n spark2 driverui
+#kubectl delete -f deployement/spark.yaml
+#kubectl delete -f deployement/spark2.yaml 
+kubectl delete service -n spark driverui 
+kubectl delete service -n spark2 driverui
 kubectl delete -f deployement/visualisation.yaml
 kops delete cluster ${CLUSTER_NAME} --yes
 sed -i 's/'"${BUCKET}"'/xxxxxxxxxxx/' ${CLUSTER_NAME}.yaml
